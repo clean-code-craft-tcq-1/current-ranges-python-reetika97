a5dba73c0f6adff3ea9d56e151e40744e5d318ab
reetika97 <77428788+reetika97@users.noreply.github.com>
2021-04-27 14:34:51 +0530

    Create currentRangesTest.py

---
+++ currentRangesTest.py
@@ -0,0 +1,10 @@
+import unittest
+import currentRanges
+
+class CurrentRangesTest(unittest.TestCase):
+  def test_count_min(self):
+    self.assertTrue(typewise_alert.count_min([4,4,5,5,4,8]) == [4,3])
+
+
+if __name__ == '__main__':
+  unittest.main()
