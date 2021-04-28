b9b30be38b9819c6ad05d3e6b8663d7650587fc4
reetika97 <77428788+reetika97@users.noreply.github.com>
2021-04-28 17:11:24 +0530

    Update currentRangesTest.py

--- currentRangesTest.py
+++ currentRangesTest.py
@@ -4,7 +4,12 @@ import currentRanges
 class CurrentRangesTest(unittest.TestCase):
   def test_count_min(self):
     self.assertTrue(currentRanges.count_min([4,4,5,5,4,8]) == [4,3])
-
-
+  def test_remove_min(self):
+    self.assertTrue(currentRanges.remove_min([4,4,5,5,4,8]) == [5,5,8])
+  def test_def_range_count(self):
+    self.assertTrue(currentRanges.def_range_count([4,4,5,5,4,8]) == [5,{4:5}])
+  def test_def_all_ranges(self):
+    self.assertTrue(currentRanges.def_def_all_ranges([1,2,2,2,4,4,5,5,4,8,9,10]) == [[4,{1:2}],[5,{4:5}],[3,{8:10}]])
+   
 if __name__ == '__main__':
   unittest.main()
